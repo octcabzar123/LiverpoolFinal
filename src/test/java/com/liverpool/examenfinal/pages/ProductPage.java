b5e830e8d187bd34214904e7d3cd3e3f688e9773
Erica Elizabeth Izquierdo Diaz <erica.izquierdo@astrazeneca.com>
2020-07-03 17:18:55 -0500

    subiendo productpage

--- src/test/java/com/liverpool/examenfinal/pages/ProductPage.java
+++ src/test/java/com/liverpool/examenfinal/pages/ProductPage.java
@@ -1,5 +1,36 @@
 package com.liverpool.examenfinal.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class ProductPage {
+    // atributos
+
+
+    //WebElement = listaDeArticulos;
+
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    //Constructor
+    public ProductPage(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    public void verificarListaArticulos(){
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".o-listing__products")));
+
+    }
+
+    public void agregarArticulos(){
+
+
+    }
+
 
 }
